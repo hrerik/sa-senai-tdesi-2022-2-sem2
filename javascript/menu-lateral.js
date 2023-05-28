aa20789f5037fc5d6f14b6b45eb9598542e08b1c
Erik <radtke.erik@gmail.com>
2023-05-28 19:06:21 -0300

    Menu lateral feito

---
+++ javascript/menu-lateral.js
@@ -0,0 +1,9 @@
+const menuLateral = document.getElementById("menu-lateral");
+
+function exibirMenuLateral() {
+  menuLateral.style.display = "block";
+}
+
+function esconderMenuLateral() {
+  menuLateral.style.display = "none";
+}
