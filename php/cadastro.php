055e1952224f8b873e1f1006db77ba69bf996926
devlongen <125667107+devlongen@users.noreply.github.com>
2023-05-29 10:59:16 -0300

    Update cadastro.php
    Só falta inserir o negocio para cadastrar

--- php/cadastro.php
+++ php/cadastro.php
@@ -9,74 +9,100 @@
 
     <style>
  body {
-        background-color: #f2f2f2;
-        font-family: Arial, sans-serif;
-        
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      display: flex;
+      padding-top: 200px;
+      justify-content: center;
+      background-color: rgb(223, 221, 221);
+      background-image: url("https://i0.wp.com/diariorp.com.br/wp-content/uploads/2018/08/fundo-login-diario.jpg?fit=300%2C168&ssl=1");
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      color: #fff;
+      text-align: center;
+    }
+
+    .digitar-sozinho {
+      font-size: 20px;
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      overflow: hidden;
+      white-space: nowrap;
+      animation: digitar 4s steps(60, end) forwards;
+    }
+
+    @keyframes digitar {
+      from {
+        width: 0
       }
 
-      .container {
-        background-color: #fff;
-        max-width: 400px;
-        margin: 0 auto;
-        padding: 20px;
-        border-radius: 5px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+      to {
+        width: 100%
       }
+    }
 
-      h1 {
-        text-align: center;
-        margin-bottom: 20px;
-      }
+    div {
+      width: 500px;
+      height: 300px;
+      border: 2px solid black;
+      display: block;
+      background-color: rgb(223, 221, 221);
+      background-image: url("https://i.pinimg.com/236x/4f/b7/35/4fb735450ccd37f844681e08fbaca7f8.jpg");
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      color: #fff;
+      text-align: center;
 
-      label {
-        display: block;
-        margin-bottom: 5px;
-      }
+    }
 
-      input {
-        display: block;
-        width: 100%;
-        padding: 10px;
-        border-radius: 5px;
-        border: none;
-        margin-bottom: 10px;
-      }
+    form {
+      text-align: center;
+      padding-top: 50px;
 
-      button {
-        background-color: #4CAF50;
-        color: #fff;
-        border: none;
-        border-radius: 5px;
-        padding: 10px 20px;
-        font-size: 16px;
-        cursor: pointer;
-        margin-top: 10px;
-      }
+    }
 
-      button:hover {
-        background-color: #3e8e41;
-      }
+    label {
+      font-size: large;
+      display: flex;
+      justify-content: center;
+      color: rgb(255, 255, 255);
+      flex-direction: row;
+    }
 
-      .mensagem-erro {
-        color: #f00;
-        font-weight: bold;
-        margin-top: 10px;
-      }
+    input {
+      width: 50%;
+      height: 15px;
+      border: none;
+      border-radius: 5px;
+      margin-bottom: 5px;
+    }
 
-      .digitar-sozinho {
-  font-size: 24px;
-  font-family: monospace;
-  overflow: hidden;
-  white-space: nowrap;
-  border-right: .15em solid orange;
-  border-left: .15em solid orange;
-  animation: digitar 2s steps(40, end) forwards;
-}
+    .botao_login {
+      width: 100px;
+      padding: 5px;
+      border: none;
+      border-radius: 5px;
+      background-color: #11e4d2;
+      color: rgb(0, 0, 0);
+      cursor: pointer;
+      padding-left: 20px;
+      margin-top: 10px;
+      margin-left: 350px;
+    }
 
-@keyframes digitar {
-  from { width: 0 }
-  to { width: 100% }
-}
+    button {
+      display: flex;
+      flex-direction: row;
+    }
 
     </style>
 </head>
@@ -84,13 +110,20 @@
     <div class="container">
         <form action="inserir_usuario.php" method="post">
         <h1 class="digitar-sozinho"></h1>
-        <label for="nome">nome:</label>
+        <label for="nome">Seu usuário:</label>
         <input type="text" id="nome" name="nome_usuario">
-        <label for="senha">senha:</label>
+        <label for="senha">Sua Senha:</label>
         <input type="password" id="senha" name="senha_usuario">
-            <button type="submit" id="botao-login" name="submit">login </button>
+            <button type="submit" id="botao-login" name="submit" class="botao_login">Sign Up!</button>
     </form>
     </div>
+    <script>
+        window.onload = function () {
+          const msg = "Olá, Seja bem-vindo faça seu Cadastro!";
+          const sozinho = document.querySelector('.digitar-sozinho');
+          sozinho.textContent = msg;
+        }
+      </script>
     
 </body>
 </html>
