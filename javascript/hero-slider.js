9f894e3e4e1c62816472c211e2f9559990e0adcd
lucasyares <jonnyney@gmail.com>
2023-05-30 17:10:26 -0300

    Atualização 2.0

--- javascript/hero-slider.js
+++ javascript/hero-slider.js
@@ -1,17 +1,13 @@
-
-
 ///SLIDERS
 $('.single-item').slick({
     infinite: true,
     autoplay: true,
-    autoplaySpeed: 1000,
+    autoplaySpeed: 3000,
     dots: true,
     arrows: false,
     slidesToScroll: 1,
     slidesToShow: 1,
 });
-
-
 //FIM SLIDERS
 
 /////ADIÇÃO DE TEXTO
@@ -22,16 +18,4 @@ botao_login.textContent = mensagem_login;
 var botao_cadastro = document.querySelector('.area-login__cadastrar');
 mensagem_cadastro = "Cadastre-se"; ///Texto do CADASTRO"
 botao_cadastro.textContent = mensagem_cadastro;
-
 /////FIM ADIÇÃO DE TEXTO
-
-
-
-////COMEÇO DAS FUNÇÕES 
-function login() {
-    window.location.href = "http://localhost/SA/Login_Luxida.php";
-}
-function cadastrar() {
-    window.location.href = "LINKDOSITE.NGROK" //TE DIRECIONA PARA A PÁGINA DE CADASTRO QUANDO CLICADA NO BOTÃO 
-}
-        ////FIM DAS FUNÇÕES
---
+++ javascript/redirecionamento.js
@@ -0,0 +1,15 @@
+function home(){
+    window.location.href = "home.html" //TE DIRECIONA PARA A PÁGINA DE INICIAL QUANDO CLICADA NO BOTÃO 
+}
+
+function login() {
+        window.location.href = "http://localhost/SA/Login_Luxida.php"; //TE DIRECIONA PARA A PÁGINA DE LOGIN QUANDO CLICADA NO BOTÃO 
+}
+
+function cadastrar() {
+        window.location.href = "LINKDOSITE.NGROK" //TE DIRECIONA PARA A PÁGINA DE CADASTRO QUANDO CLICADA NO BOTÃO 
+}
+
+function url(idDiv) {
+    window.location.href = 'Produtos.html?id=' + idDiv; //PÁGINA DO PRODUTO
+}
