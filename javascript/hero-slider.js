5da6474b7e6f1e13c1881a01d102da5acd0836f4
Erik <erik_radtke@estudante.sesisenai.org.br>
2023-05-31 14:36:28 -0300

    Modificações pequenas

--- javascript/hero-slider.js
+++ javascript/hero-slider.js
@@ -9,13 +9,3 @@ $('.single-item').slick({
     slidesToShow: 1,
 });
 //FIM SLIDERS
-
-/////ADIÇÃO DE TEXTO
-var botao_login = document.querySelector(".area-login__entrar")
-mensagem_login = "Login"; ///Texto do LOGIN"
-botao_login.textContent = mensagem_login;
-
-var botao_cadastro = document.querySelector('.area-login__cadastrar');
-mensagem_cadastro = "Cadastre-se"; ///Texto do CADASTRO"
-botao_cadastro.textContent = mensagem_cadastro;
-/////FIM ADIÇÃO DE TEXTO
