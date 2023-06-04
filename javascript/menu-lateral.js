2cdbc43bea5c79c1b4475d1a7724c219a2ad2b9a
Erik <radtke.erik@gmail.com>
2023-06-04 14:14:40 -0300

    Menus laterais prontos

--- javascript/menu-lateral.js
+++
@@ -1,9 +0,0 @@
-const menuLateral = document.getElementById("menu-lateral");
-
-function exibirMenuLateral() {
-  menuLateral.style.display = "block";
-}
-
-function esconderMenuLateral() {
-  menuLateral.style.display = "none";
-}
