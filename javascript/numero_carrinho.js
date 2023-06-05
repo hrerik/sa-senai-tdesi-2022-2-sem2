45bcc5a2bcf35421aed1d7df7fb6e48f1f65dc1d
lucasyares <125934837+lucasyares@users.noreply.github.com>
2023-06-04 22:31:56 -0300

    erro arrumado

--- javascript/numero_carrinho.js
+++ javascript/numero_carrinho.js
@@ -1,8 +1,7 @@
 
 function aumentarNumero() {
   var numero_car = document.getElementById('carrinho-quantidade');
-  var numeroAtual = parseInt(numero_car.textContent);
+  var numeroAtual = parseInt(numero_car.textContent) ;
   numero_car.textContent = numeroAtual + 1;
 }
 
-aumentarNumero();
